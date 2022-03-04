cfb114200784d6bfd1273001be9d7eb137d37e3d
alexdan93 <alek9309@mail.ru>
2022-03-04 13:53:35 +0700

    add model connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { ProductModule } from './product/product.module';
 import { TopPageModule } from './top-page/top-page.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthModule } from './auth/auth.module';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
         AuthModule,
         TopPageModule,
         ProductModule,
         ReviewModule,
     ],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
